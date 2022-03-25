806f05ad46ac1f6464b8a342e69e2ff73c66f4a4
Miguel_Reis <guelsouzar@gmail.com>
2022-03-24 22:39:39 -0300

    req 1

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const strangerThingsRepository = new StrangerThingsRepository(
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
-// escrever algo para fazer o commit e assim fazer o  pull request
+
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const { PORT } = process.env;
+
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
